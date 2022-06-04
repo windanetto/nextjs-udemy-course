9787c2134ebf9abbeb9b62f00873c42b8e34f28c
windaAgusthia <windaagusthia@campaign.com>
2022-06-04 13:28:03 +0700

    add event-detail and wrap apps with layout header

---
+++ components/layout/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Fragment } from 'react'
+import MainHeader from './main-header'
+
+export default function Layout(props) {
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+    </Fragment>
+  )
+}
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,27 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import EventContent from "../../components/event-detail/event-content";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+import { getEventById } from "../../dummy-data";
+
 
 export default function EventDetailPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return <p>No event found!</p>
+  }
+
   return (
-    <div>EventDetailPage</div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics date={event.date} address={event.location} image={event.image} imageAll={event.title} />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   )
 }
